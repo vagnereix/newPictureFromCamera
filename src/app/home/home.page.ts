eac4656cdf2dff14c8ecc65fff6f70731a169217
Vagner Reis <vagner.reis@urbana-pe.com.br>
2021-04-06 11:25:50 -0300

    Ready for new photo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Plugins, CameraResultType } from '@capacitor/core';
+const { Camera } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  guestPicture: string;
 
   constructor() {}
+
+  async takePicture() {
+    try {
+      const profilePicture = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Base64,
+      });
+      this.guestPicture = profilePicture.base64String;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 }
